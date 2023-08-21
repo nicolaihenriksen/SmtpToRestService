eecb8c81973cc9aa9aed7e5e225accdb7f61ab7c
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-08-21 22:41:59 +0200

    Fix artifact upload content

--- SmtpToRest.WindowsService/Publish.ps1
+++ SmtpToRest.WindowsService/Publish.ps1
@@ -1,3 +1,2 @@
 dotnet publish ./SmtpToRest.WindowsService/SmtpToRest.WindowsService.csproj -p:Configuration=Release -p:DebugType=None -p:DebugSymbols=false -o:./SmtpToRest.WindowsService/publish
 rm ./SmtpToRest.WindowsService/publish/appsettings.Development.json
-Compress-Archive -Path ./SmtpToRest.WindowsService/publish/* -DestinationPath ./SmtpToRest.WindowsService/publish/SmtpToRest.WindowsService.zip
