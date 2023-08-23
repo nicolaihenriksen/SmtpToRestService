1a04cde8778b8029aadee21f8c964e77eb32fe70
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-08-23 12:50:37 +0200

    Use top-level statements in Program.cs for windows service project.

--- SmtpToRest.WindowsService/Program.cs
+++ SmtpToRest.WindowsService/Program.cs
@@ -1,24 +1,17 @@
 using System.IO;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SmtpToRest;
 using SmtpToRest.Config;
 
-namespace SmtpToRest.WindowsService;
+var builder = Host.CreateDefaultBuilder(args)
+	.ConfigureServices((_, services) =>
+	{
+		services
+			.UseSmtpToRestDefaults()
+			.AddSingleton<IConfigurationProvider>(sp => new ConfigurationProvider(() => Path.Combine(System.AppContext.BaseDirectory)));
+	})
+	.UseWindowsService();
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureServices((_, services) =>
-            {
-	            services
-		            .UseSmtpToRestDefaults()
-		            .AddSingleton<IConfigurationProvider>(sp => new ConfigurationProvider(() => Path.Combine(System.AppContext.BaseDirectory)));
-			})
-            .UseWindowsService();
-}
+var app = builder.Build();
+app.Run();
