446fbaa8647b76c6414e309c3c33e0ddad28fd76
Nicolai Henriksen <nicolai.h80@gmail.com>
2023-09-02 23:18:43 +0200

    PowerShell script content placed directly in pipeline step

--- SmtpToRest.WindowsService/Publish.ps1
+++
@@ -1,6 +0,0 @@
-param(
-  [string]$BuildVersion = "1.0.0.0"
-)
-
-dotnet publish ./SmtpToRest.WindowsService/SmtpToRest.WindowsService.csproj -p:Configuration=Release -p:BuildVersion=$BuildVersion -p:DebugType=None -p:DebugSymbols=false -f net7.0 -o:./SmtpToRest.WindowsService/publish
-rm ./SmtpToRest.WindowsService/publish/appsettings.Development.json
