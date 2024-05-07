1f9792e79e3c6aa4277f7007edcbbbfd6c3b7918
Nicolai Henriksen <nicolai.h80@gmail.com>
2024-05-07 16:04:03 +0200

    Implement feature

---
+++ SmtpToRest/Rest/Decorators/HttpClientNameDecorator.cs
@@ -0,0 +1,12 @@
+﻿using SmtpToRest.Config;
+using SmtpToRest.Services.Smtp;
+
+namespace SmtpToRest.Rest.Decorators;
+
+internal class HttpClientNameDecorator : DecoratorBase, IRestInputDecorator
+{
+	public void Decorate(RestInput restInput, ConfigurationMapping mapping, IMimeMessage message)
+	{
+		restInput.HttpClientName = mapping.CustomHttpClientName;
+	}
+}
